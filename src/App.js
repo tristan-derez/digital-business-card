19a88f9952642b349e6b143643e2455a34cd9ef3
dreyzu <tristan.derez@gmail.com>
2022-02-10 12:50:40 +0100

    feat: :sparkles: add ChakraProvider and initialize colorMode to dark

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
+const theme = extendTheme({ config: { initialColorMode: "dark" } });
+
 export default function App() {
   return (
-    <Flex justifyContent='center'>
-      <Flex direction='column' justifyContent='center' mt='44px'>
-        <Box borderRadius='10px' overflow='hidden' w='317px' bg='#1A1B21'>
-          <Header />
-          <Main />
-          <Footer />
-        </Box>
+    <ChakraProvider theme={theme}>
+      <Flex justifyContent='center'>
+        <Flex direction='column' justifyContent='center' mt='44px'>
+          <Box borderRadius='10px' overflow='hidden' w='317px' bg='#1A1B21'>
+            <Header />
+            <Main />
+            <Footer />
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </ChakraProvider>
   );
 }
